fe35874f29937763895b5d4bac51f973376a8868
sakib-xrz <sakibxrz21@gmail.com>
2026-01-31 12:56:45 +0600

    refactor(auth): reorganize JWT module registration and remove redundant imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core/constants';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { RolesGuard } from './auth/guards/roles.guard';
@@ -16,18 +15,6 @@ import { RolesGuard } from './auth/guards/roles.guard';
       isGlobal: true,
     }),
     PrismaModule,
-    JwtModule.registerAsync({
-      global: true,
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: parseInt(
-            configService.get<string>('JWT_ACCESS_TOKEN_TTL') ?? '3600',
-          ),
-        },
-      }),
-      inject: [ConfigService],
-    }),
     AuthModule,
     UsersModule,
   ],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,25 @@ import { AuthService } from './providers/auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { ConfigService } from '@nestjs/config';
 import { BcryptProvider } from './providers/bcrypt.provider';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: parseInt(
+            configService.get<string>('JWT_ACCESS_TOKEN_TTL') ?? '3600',
+          ),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [ConfigService, AuthService, BcryptProvider],
-  imports: [UsersModule],
 })
 export class AuthModule {}
